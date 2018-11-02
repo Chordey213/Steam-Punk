979fd1c0858474c7e90591f6c6b7dbcb1ffb5c09
Chordey213 <cpwalla026@gmail.com>
2018-11-01 22:27:08 -0400

    setup the wireframe and began planning for the UATU App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
-
+import './App.css';
+import './img/uatu1.jpg';
+// MVP A Marvel character and comic book search page, like google but for Marvel comics //
 class App extends Component {
-  render() {
-    return (
-      <div>
+  constructor(props) {
+    super()
+    // state = [];
 
-      </div>
+  }
+  render() {
+    // Left hand side of page is Uatu the watcher (picture) 
+    // Top of the page is the Marvel character lineup image
+    // main page content is a search bar, with a description of what you can do
+
+    return (
+      <table className="uatu">
+        <tbody>
+          <tr className="top">
+            <td rowSpan="2" className="one"><img className="img" src="https://via.placeholder.com/100x350.png?text=UATU+goes+here" alt="shit"></img></td>
+            <td colSpan="2"><img className="img" src="https://via.placeholder.com/780x100.png?text=Marvel+Banner+goes+here" alt="shit"></img></td>
+          </tr>
+          <tr>
+            <td className="two"><img className="img" src="https://via.placeholder.com/730x400.png?text=SEARCH+goes+here" alt="shit"></img></td>
+          </tr>
+        </tbody>
+      </table>
     );
   }
 }
